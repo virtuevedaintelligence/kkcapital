2b6ccaab90e78104417807cff444313616d3d34b
kolambkar11 <root@VVI.local>
2023-11-21 00:40:28 +0530

    final changes done

---
+++ git-deploy.sh
@@ -0,0 +1,16 @@
+  #!/bin/bash
+
+# Print git status
+git status
+
+# Add all changes to the staging area
+git add .
+
+# Commit changes with a custom message
+git commit -m "final changes done"
+
+# Push the changes to the remote repository
+git push
+
+# Run npm command to deploy
+npm run deploy
--- src/components/Headersection.js
+++ src/components/Headersection.js
@@ -1,8 +1,8 @@
 import React from "react";
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Disclosure } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const navigation1 = [
   { name: "About", href: "#about" },
