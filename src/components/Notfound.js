bf6d48eafe62b7b1be37eba81193537f2e156894
kolambkar11 <root@VVI.local>
2023-11-21 00:27:54 +0530

    comment added in notfound

--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Notfound = () => {
+  // notfound page
   return (
     <>
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
