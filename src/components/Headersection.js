9d0a8b59585c3e074568d1872ce1c9493bebdde1
virtuevedaintelligence <virtuevedaintelligence@gmail.com>
2023-11-24 11:07:51 +0530

    final changes done

--- src/components/Headersection.js
+++ src/components/Headersection.js
@@ -82,7 +82,8 @@ const Headersection = () => {
             {/* Replace with your content */}
             <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
               <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
-                <Imageslider />
+                {/* <Imageslider /> */}
+                Image Slider comes here
               </div>
             </div>
             {/* /End replace */}
